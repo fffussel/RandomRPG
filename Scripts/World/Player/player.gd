614960e4d23ab5738643ffcae5d591f3742a7599
Lucas Schladitz <lsl@hup.de>
2026-08-24 10:02:28 +0200

    fuck it, we ball

--- Scripts/World/Player/player.gd
+++ Scripts/World/Player/player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
+@onready var playerSprite: AnimatedSprite2D = $PlayerSprite
 
-const SPEED = 100
+const SPEED = 125
+
+var animDir: int = 0
+var animWalk: bool = false
 
 
 func _physics_process(delta: float) -> void:
@@ -10,12 +14,46 @@ func _physics_process(delta: float) -> void:
 	
 	if directionWE:
 		velocity.x = directionWE * SPEED
+		animWalk = true
+		if directionWE > 0:
+			animDir = 3
+		if directionWE < 0:
+			animDir = 1
 	else:
 		velocity.x = 0
 	
 	if directionNS:
 		velocity.y = directionNS * SPEED
+		animWalk = true
+		if directionNS > 0:
+			animDir = 0
+		if directionNS < 0:
+			animDir = 2
 	else:
 		velocity.y = 0
+		
+	if directionNS == 0 && directionWE == 0:
+		animWalk = false
 	
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	animProcess()
+
+func animProcess():
+	var animName
+	if animWalk:
+		animName = "walk_"
+	else:
+		animName = "idle_"
+	
+	if animDir == 0:
+		animName += "S"
+	elif animDir == 1:
+		animName += "E"
+	elif animDir == 2:
+		animName += "N"
+	elif animDir == 3:
+		animName += "W"
+	
+	playerSprite.play(animName)
