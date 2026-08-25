51f8210c606e8956ccc341393ea10a793f43481b
Lucas Schladitz <lsl@hup.de>
2026-08-25 08:44:59 +0200

    even more stats

--- Resources/Player/Stats/statsScript.gd
+++ Resources/Player/Stats/statsScript.gd
@@ -2,6 +2,9 @@ class_name PlayerStats
 extends Resource
 
 @export var name: String
+@export var lvl: int
+@export var exp: float
+@export var exp4NextLVL: float
 @export var speed: float = 125
 @export var strength: float
 @export var luck: float
@@ -9,5 +12,18 @@ extends Resource
 @export var critChance: float
 @export var critMult: float
 @export var charisma: float
-@export var inventory: Array
+@export var inventory: Array #TODO specify type
 @export var maxInventorySize: int
+@export var attacks: Array #TODO specify type
+@export var money: int
+@export var maxMoney: int
+@export var equippedHead: Resource #TODO specify type
+@export var equippedTorso: Resource #TODO specify type
+@export var equippedLegs: Resource #TODO specify type
+@export var equippedFeet: Resource #TODO specify type
+@export var equippedWeapon: Resource #TODO specify type
+@export var equippedCharm: Resource #TODO specify type
+@export var HP: float
+@export var maxHP: float
+@export var magic: float
+@export var maxMagic: float
