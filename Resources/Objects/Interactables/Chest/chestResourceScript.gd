e5d7cb29f67bad0878822caec6a743341672c105
Lucas Schladitz <lsl@hup.de>
2026-08-27 10:33:22 +0200

    hznhghhjhfhjjhkhgftl4

---
+++ Resources/Objects/Interactables/Chest/chestResourceScript.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+@export var name: String
+@export var items: Array
+@export var maxSpace: int
+@export var sprite: SpriteFrames
+const animNameOpen = "chest_open"
+const animNameClosed = "chest_closed"
