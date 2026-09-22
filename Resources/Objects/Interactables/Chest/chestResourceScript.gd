bbc8d7b26aa7b30be193bb03d5245f7452af1987
Lucas Schladitz <lsl@hup.de>
2026-09-22 13:26:04 +0200

    chest opening and closing

--- Resources/Objects/Interactables/Chest/chestResourceScript.gd
+++ Resources/Objects/Interactables/Chest/chestResourceScript.gd
@@ -4,6 +4,7 @@ extends Resource
 @export var name: String
 @export var items: Array
 @export var maxSpace: int
-@export var sprite: SpriteFrames
+@export var wasOpened: bool
+@export var randomized: bool
 const animNameOpen = "chest_open"
 const animNameClosed = "chest_closed"
--- Scripts/World/Interactables/Chests/Solid/wooden_chest.gd
+++ Scripts/World/Interactables/Chests/Solid/wooden_chest.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var res: ChestResource
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
 func _ready() -> void:
-	animated_sprite_2d.sprite_frames = res.sprite
+	if res.wasOpened:
+		animated_sprite_2d.animation = res.animNameClosed
+	else :
+		animated_sprite_2d.animation = res.animNameClosed
